eb1ff593ea0ed4a81c4bd5ec0d20d16277b8609b
sanhltt1999 <sanhlt@maxmobile-software.com>
2021-05-18 11:18:33 +0700

    load data to profile in home activity

--- app/src/main/java/com/intern/cndd/ui/home/HomeActivity.java
+++ app/src/main/java/com/intern/cndd/ui/home/HomeActivity.java
@@ -3,14 +3,32 @@ package com.intern.cndd.ui.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.intern.cndd.R;
+import com.intern.cndd.prevalent.Prevalent;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private ShapeableImageView mAvatarImageView;
+    private TextView mTitleTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAvatarImageView = findViewById(R.id.avatarImageView);
+        mTitleTextView = findViewById(R.id.titleTextView);
+
+        mTitleTextView.setText("Hi, " + Prevalent.currentOnlineUser.getName() + "!");
+        Glide
+                .with(this)
+                .load(Prevalent.currentOnlineUser.getPicture())
+                .centerCrop()
+                .into(mAvatarImageView);
     }
 }
